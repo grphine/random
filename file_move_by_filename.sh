aace261d300972a940bd183917a93c776bd74b15
grphine <grphine@gmail.com>
2024-10-25 22:51:52 +0100

    Update file_move_by_filename.sh

--- file_move_by_filename.sh
+++ file_move_by_filename.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 <<'###'
+0. get current dir name (e.g. "ash")
 1. list files
 2. awk unique occurrences of second column
    e.g. 
@@ -11,16 +12,20 @@
    will result in
      201733
      195204
-3. loop over all lines of stdout
-4. skip empty lines
-5. move file into folder matching col name (folder already exists)
+3. loop over all lines of stdout, and assign current line to $id
+4. ignore any empty lines
+5. create folder (ignore if it already exists)
+6. move file into folder matching col name
    e.g. 
      ash_201733_044BMB_201733090.jpg -> 201733/ash_201733_044BMB_201733090.jpg
      ash_195204_044ASI_195204001.jpg -> 195204/ash_195204_044ASI_195204001.jpg
 ###
 
-ls | awk -F"_" '{a[$2]++} END { for (b in a) {print b} }' | while read -r line; do
-if [ -n "$line" ]; then
-mv -vi ash*$line* $line/
-fi;
+folder=$(echo "${PWD##*/}")
+ls | awk -F"_" '{a[$2]++} END { for (b in a) {print b} }' | while read -r id; do
+  if [ -n "$id" ]; then
+    mkdir -p $id
+    mv -vi $folder*$id* $id/
+  fi;
 done;
+
