e9a7e874459629e5173d8071d3efd31d071a5daf
grphine <grphine@gmail.com>
2024-10-26 15:14:58 +0100

    Update file_move_by_filename.sh

--- file_move_by_filename.sh
+++ file_move_by_filename.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 <<'###'
-0. get current dir name (e.g. "ash")
-1. list files
-2. awk unique occurrences of second column
+1. for each dir in current "parent" dir
+2. enter dir
+3. get current dir name (e.g. "ash")
+4. list files
+5. awk unique occurrences of second column
    e.g. 
      ├── ash_201733_044BMB_201733090.jpg 
      ├── ash_201733_044BMB_201733134.jpg 
@@ -12,20 +14,31 @@
    will result in
      201733
      195204
-3. loop over all lines of stdout, and assign current line to $id
-4. ignore any empty lines
-5. create folder (ignore if it already exists)
-6. move file into folder matching col name
+6. loop over all lines of stdout, and assign current line to $id
+7. ignore any empty lines
+8. create folder (ignore if it already exists)
+9. move file into folder matching col name
    e.g. 
      ash_201733_044BMB_201733090.jpg -> 201733/ash_201733_044BMB_201733090.jpg
      ash_195204_044ASI_195204001.jpg -> 195204/ash_195204_044ASI_195204001.jpg
+10. exit dir, goto 1
 ###
 
-folder=$(echo "${PWD##*/}")
-ls | awk -F"_" '{a[$2]++} END { for (b in a) {print b} }' | while read -r id; do
-  if [ -n "$id" ]; then
-    mkdir -p $id
-    mv -vi $folder*$id* $id/
-  fi;
+file_mover () {
+   folder=$(echo "${PWD##*/}")
+   ls | awk -F"_" '{a[$2]++} END { for (b in a) {print b} }' | while read -r line; do
+      if [ -n "$line" ]; then
+         mkdir -p $line
+         mv -i $folder*$line* $line/
+      fi;
+   done;
+}
+
+for D in ./*; do
+    if [ -d "$D" ]; then
+        cd "$D"
+        file_mover
+        cd ..
+    fi;
 done;
 
